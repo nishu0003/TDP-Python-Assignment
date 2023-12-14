17a541ff125e96cea664b251ad6b94b32a036882
NChauhan3112 <99498373+nishu0003@users.noreply.github.com>
2023-12-14 21:44:36 +0530

    Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -26,9 +26,9 @@ cur=mydb.cursor()
 #cur.execute("CREATE DATABASE db1")
 
 """s="CREATE TABLE Student_details(student_id INT(4) PRIMARY KEY,first_name VARCHAR(20),last_name VARCHAR(20),age INT(2) NOT NULL,grade FLOAT(5,2))"
-cur.execute(s)"""        #hete i made student details table and define them
+cur.execute(s)"""        #here i made student details table and define them
 
-n=int(input("How many Students data you want to add : "))             #here we will how many students data we want to add in our database.
+n=int(input("How many Students data you want to add : "))             #here we will input how many students data we want to add in our database.
 for x in range(n):                 #inside that loop we addup the n number of students data. 
     id=input("ENter the id of {} student : ".format(str(x+1)))
     fname=input("ENter the first name of student : ")
@@ -39,9 +39,9 @@ for x in range(n):                 #inside that loop we addup the n number of st
     b1=(id,fname,lname,age,grade)
     cur.execute(m,b1)
     mydb.commit()
-u="UPDATE Student_details set grade='97.0' WHERE first_name='Alice'"           #here we are updating the grades of student whose first name is Alice. 
+u="UPDATE Student_details set grade='97.0' WHERE first_name='Alice'"           #here we are updating the grades of student whose first name is 'Alice'. 
 cur.execute(u)
 mydb.commit()
-d="DELETE FROM Student_details WHERE last_name='Smith'"              #here we delete the student details whose last name is Smith.
+d="DELETE FROM Student_details WHERE last_name='Smith'"              #here we delete the student details whose last name is 'Smith'.
 cur.execute(d)
 mydb.commit()
